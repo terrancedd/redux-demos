eadb3fd35420f639a41fc3e52559b856421be660
terryc129 <terryc129@gmail.com>
2016-10-25 22:28:02 +0800

    extract Todo and TodoList components from TodoApp

--- demo02/todoapp.jsx
+++ demo02/todoapp.jsx
@@ -123,6 +123,41 @@ const FilterLink=({filter,currentFilter,children})=>{
 };
 
 
+
+const Todo=({onClick,
+             completed,
+             text
+      })=>(
+  <li 
+    onClick={onClick}
+    style={{textDecoration:completed?'line-through':'none'}}
+  >
+     {text}
+  </li>
+
+);
+
+const TodoList=({
+      todos,
+      onTodoClick
+      })=>(
+      <ul>
+        {todos.map(todo =>
+            <Todo
+              key={todo.id}
+              {...todo}
+              onClick={()=>onTodoClick(todo.id)}
+            />
+            )
+        }
+      </ul>
+
+      );
+
+
+
+
+
 const getVisibleTodos=(
         todos,
         filter
@@ -138,7 +173,7 @@ const getVisibleTodos=(
         }
 
 
-}
+};
 
 
 
@@ -168,19 +203,15 @@ class TodoApp extends Component {
              }}>
                 Add Todo
             </button>
-            <ul>
-              {
-                visibleTodos.map(todo=>
-                    <li key={todo.id} 
-                        onClick={()=>store.dispatch({type:'TOGGLE_TODO',id:todo.id})}
-                        style={{textDecoration:todo.completed?'line-through':'none'}}
-                    >
-                      
-                        {todo.text}
-                       
-                    </li>
-               )}
-            </ul>
+            <TodoList
+                todos={visibleTodos}
+                onTodoClick={id =>
+                    store.dispatch({
+                        type:'TOGGLE_TODO',
+                        id
+                    })
+                }
+            />
             <p>
                 Show:
                 {' '}
