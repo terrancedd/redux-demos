ee7f3a7aa9f8f75f651ca616a0bb176dae7f3adc
terryc <terryc129@gmail.com>
2016-11-09 22:30:09 +0800

    .

--- demo02/src/index.js
+++ demo02/src/index.js
@@ -5,17 +5,20 @@ import { Provider } from 'react-redux'
 import App from './components/App'
 import reducer from './reducers'
 import {loadState,saveState} from './localStorage'
+import throttle from 'lodash/throttle'
+
+
 const persistedState= loadState();
 
 //use the second arguments for the initial state
 const store = createStore(reducer,persistedState)
 
 //save state to localStorage once changed
-store.subscribe(()=>{
+store.subscribe(throttle(()=>{
 
 	saveState({todos:store.getState().todos});
 
-})
+},1000))
 
 render(
  <Provider store={store}>
