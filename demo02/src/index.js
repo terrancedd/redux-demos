372e3ae62d7bf405c177ebdd2a415948610900d7
terryc <terryc129@gmail.com>
2016-11-09 22:04:33 +0800

    .

--- demo02/src/index.js
+++ demo02/src/index.js
@@ -4,18 +4,18 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import reducer from './reducers'
-
-const persistedState= {
-	todos:[{
-		id:'0',
-		text:'Welcome back!',
-		completed:false,
-	}],
-}
+import {loadState,saveState} from './localStorage'
+const persistedState= loadState();
 
 //use the second arguments for the initial state
 const store = createStore(reducer,persistedState)
 
+//save state to localStorage once changed
+store.subscribe(()=>{
+
+	saveState({todos:store.getState().todos});
+
+})
 
 render(
  <Provider store={store}>
