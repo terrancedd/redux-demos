8c315b3e494d2485b1b9bb92017b8f86f7c88f98
terryc129 <terryc129@gmail.com>
2016-11-13 21:52:49 +0800

    .

--- demo02/src/actions/index.js
+++ demo02/src/actions/index.js
@@ -25,3 +25,9 @@ export const fetchTodos=filter=>
 	api.fetchTodos(filter).then(response=>
 			receiveTodos(filter,response)
 		);
+
+
+export const requestTodos=filter=>({
+	type:'REQUEST_TODOS',
+	filter,
+})
